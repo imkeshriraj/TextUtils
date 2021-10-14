5a3a944627a497e06522a15669fdf52bc0622427
Keshriraj7870 <adityabkrajs@gmail.com>
2021-10-14 11:23:30 +0530

    commits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 
@@ -12,14 +18,14 @@ import Alert from './components/Alert';
 function App() {
 
   const [mode, setMode] = useState('light'); //whether dark mode enable or not
- 
+
   const [alert, setAlert] = useState(null);
 
-  const showAlert =(message,type)=>{
+  const showAlert = (message, type) => {
 
     setAlert({
-      msg:message,
-      type:type
+      msg: message,
+      type: type
     })
 
     setTimeout(() => {
@@ -27,26 +33,26 @@ function App() {
     }, 1500);
 
   }
-  const toggleMode = ()=>{
-    if(mode =='light'){
+  const toggleMode = () => {
+    if (mode == 'light') {
       setMode('dark');
-      document.body.style.backgroundColor='#042743';
-      showAlert('Dark Mode has been Enabled',"success");
-      document.title=`TextUtil's- Dark Mode`;
+      document.body.style.backgroundColor = '#042743';
+      showAlert('Dark Mode has been Enabled', "success");
+      document.title = `TextUtil's- Dark Mode`;
       // setInterval(() => {
-        
+
       //   document.title=`TextUtils is amazing`;
       // }, 2000);
       // setInterval(() => {
-        
+
       //   document.title=`Install it TextUtils `;
       // }, 1500);
     }
-    else{
+    else {
       setMode('light');
-      document.body.style.backgroundColor='white';
-      showAlert('Light Mode has been Enabled',"success");
-      document.title=`TextUtil's- Light Mode`;
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light Mode has been Enabled', "success");
+      document.title = `TextUtil's- Light Mode`;
 
 
 
@@ -54,15 +60,26 @@ function App() {
   }
   return (
     <>
-<Navbar title="TestUtil's" mode={mode} aboutText="About-Us"  contact="contact-Us" toggleMode={toggleMode}/>
-<Alert  alert={alert} />
-<div className="container my-5">
-<TextForm heading="Enter the Text to Analyze Below" mode={mode} showAlert={showAlert}/>
+    <Router>
+      <Navbar title="TestUtil's" mode={mode} aboutText="About-Us" contact="contact-Us" toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-5">
 
-{/* <About/> */}
-</div>
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+         
+          <Route excat path="/">
+          
+        <TextForm heading="Enter the Text to Analyze Below" mode={mode} showAlert={showAlert} />
+          </Route>
+        </Switch>
+      </div>
+        </Router>
+       
     </>
-  ); 
+  );
 }
 
 export default App;
